91080acf7bb09917d8442a9a213bec1e87a0de1d
Diego Hervas <diego.diguin91@gmail.com>
2018-04-27 15:47:24 +0200

    Add Medtronic src files

---
+++ src/DependencyInjection/RandockMetronic5Extension.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Randock\Metronic5Bundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class RandockMetronic5Extension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+}
---
+++ src/RandockMetronic5Bundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Randock\Metronic5Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RandockMetronic5Bundle extends Bundle
+{
+
+}

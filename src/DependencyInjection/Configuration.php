60c159e8faa70c40e0f3d4a940e8baf6dd07323f
Juanjo <juanjose@randock.com>
2019-01-08 11:36:44 +0100

    fix tree builder deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,8 +11,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('randock_metronic5');
+        $treeBuilder = new TreeBuilder('randock_metronic5');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()

a66047db488237787a085b4f28285f084d1a2723
David Gonzalez <benaventebiker_007@hotmail.com>
2019-11-11 13:18:37 +0100

    test gutil

--- tools/gulp/compile.js
+++ tools/gulp/compile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var build = require('./build');
 var func = require('./helpers');
 
 // merge with default parameters
-var args = Object.assign({'prod': false}, process.env);
+var args = Object.assign({'prod': false}, gutil.env);
 
-console.log(process.env);
+console.log(gutil.env);
 
 if (args.prod !== false) {
   // force disable debug for production
